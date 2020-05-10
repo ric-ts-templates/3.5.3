536121ed19fb52dbcfc0aec9048f96bfb6ea925d
Richard PYT <rick.info.rsoc@gmail.com>
2020-05-10 18:38:51 +0200

    First new Version for __myTSC2.bat

--- _src/app/A/TS_Class1.ts
+++ _src/app/A/TS_Class1.ts
@@ -1,8 +1,5 @@
-import {TS_Class2} from "./../B/TS_Class2.js";
-
 export class TS_Class1 {
     debug() {
-        console.log('TS_Class1.debug()');
-        (new TS_Class2()).debug();
+        console.log('TS_Class1.debug() called, OK!');
     }
 }
