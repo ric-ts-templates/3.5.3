48ac0669219f2232e02c24d4abb16b809e17ec8f
Richard PYT <rick.info.rsoc@gmail.com>
2020-10-22 08:54:48 +0200

    Modif. commentaire

--- _src/app/_main.ts
+++ _src/app/_main.ts
@@ -1,8 +1,10 @@
 import {TS_Class1} from "./A/TS_Class1.js"; //Heureusement, le compilo. ne tiendra pas 
                                             //compte de l'extension écrite (".js"), 
-                                            //et lira juste "./app/TS_Class1"
-                                            //donc en l'occurence "./app/TS_Class1.ts"
+                                            //et lira juste "./A/TS_Class1"
+                                            //donc en l'occurence "./A/TS_Class1.ts"
                                             //Mais bon, c'est pas très beau non plus :/
+                                            //Intérêt: dans le code compilé (dist/...)
+                                            //         on aura bien l'extension ".js" conservée !!
 export class Main {
     public run(): void {
         let oTS_Class1: TS_Class1 = new TS_Class1();
